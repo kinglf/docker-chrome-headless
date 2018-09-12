93312a622e322a880410da11216d34f1975fbf66
周兴杰 <kinglf@live.cn>
2018-09-12 17:00:47 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-# Create NSSDB
-certutil -d sql:$NSSDB --empty-password -N -f /dev/null 2> /dev/null
-echo "Keystore created"
-
 # Run the NSSDB updating utility in background
 import_cert.sh $HOME &
 
-sleep 2
+CHROME_ARGS="--disable-gpu --headless --no-sandbox --remote-debugging-address=$DEBUG_ADDRESS --remote-debugging-port=$DEBUG_PORT --user-data-dir=/data --disable-dev-shm-usage"
 
-CHROME_ARGS="--disable-gpu --headless --no-sandbox --dbus-stub --remote-debugging-address=$DEBUG_ADDRESS --remote-debugging-port=$DEBUG_PORT --user-data-dir=/data"
-
-# Add extra options to Chrome from environment variable CHROME_OPTS
 if [ -n "$CHROME_OPTS" ]; then
   CHROME_ARGS="${CHROME_ARGS} ${CHROME_OPTS}"
 fi
 
 # Start Chrome
-echo "Starting Chrome version: ${CHROME_VERSION}"
-exec sh -c "/usr/bin/google-chrome-unstable $CHROME_ARGS"
-
+sh -c "/usr/bin/google-chrome-unstable $CHROME_ARGS"
